b6ef81029202202f1a8fafffadd98957ff70eba8
israelwangila <wangilaisrael@gmail.com>
2020-08-25 09:01:24 +0300

    Auto-generated commit

--- src/app/quote.spec.ts
+++
@@ -1,8 +0,0 @@
-import { Quote } from './quote';
-import { stringify } from 'querystring';
-
-describe('Quote', () => {
-  it('should create an instance', () => {
-    expect(new Quote('', '','')).toBeTruthy();
-  });
-});
